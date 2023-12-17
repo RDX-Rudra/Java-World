c6b50200429f3d20ad0c6e958e438f313af505cf
RDX-Rudra <dasrudra234@gmail.com>
2023-12-17 16:27:35 +0530

    let

---
+++ com.KunalDsa/BinarySearch/BinarySearch.java
@@ -0,0 +1,11 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        
+    }
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+    }
+}
