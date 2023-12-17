c18a05469a3c3888cb1be87d3af833390b145ca2
RDX-Rudra <dasrudra234@gmail.com>
2023-12-17 12:44:33 +0530

    1ghg

--- com.KunalDsa/MxnimunWealthyPerson.java
+++ com.KunalDsa/MxnimunWealthyPerson.java
@@ -1,8 +1,21 @@
 public class MxnimunWealthyPerson {
     public static void main(String[] args) {
-        
+        int[][] accounts = {{1, 2, 3},{1, 5, 3},{9, 1}};
+        System.out.println(maximimWealth(accounts));
     }
-    public int maximimWealth(int[][] accounts){
-        
+    public static int maximimWealth(int[][] accounts){
+        int maxWealthy = Integer.MIN_VALUE;
+        int i=0;
+        for(int[] person: accounts){
+            int totalAmmount = 0;
+            for(int account: person){
+                totalAmmount += account;
+            }
+            if(totalAmmount > maxWealthy){
+                maxWealthy = totalAmmount;
+                i++;
+            }
+        }
+        return maxWealthy;
     }
 }
