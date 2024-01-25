2cd18f54e38982daaca102ee6b2ecb63ea16d171
RDX-Rudra <dasrudra234@gmail.com>
2024-01-25 23:05:13 +0530

    dsa

---
+++ GFG_problems/p1_25.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class p1_25 {
+    public static int solve(int num1,int num2){
+        //code here
+        int isPrime[] = new int[10000];
+        Arrays.fill(isPrime,1);
+        for(int i =2 ;i*i<10000;i++){
+            for(int j = i*i;j<10000;j=j+i){
+                if(isPrime[j]==1){
+                    isPrime[j]=0;
+                }
+            }
+        }
+        
+        int vis[] = new int[10000];
+        int dp[] = new int[10000];
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(num1);
+        while(!q.isEmpty()){
+            int currNode = q.poll();
+            vis[currNode]=1;
+            String str = currNode+"";
+            for(int i =0 ;i<4;i++){
+                for(char c ='0';c<='9';c++){
+                    if((c=='0'&& i==0) || str.charAt(i)==c){
+                        continue;
+                    }
+                    String nextNode = str.substring(0,i)+c+str.substring(i+1);
+                   
+                    int nxtNode = Integer.parseInt(nextNode);
+                    if(isPrime[nxtNode]==1 && dp[nxtNode]==0){
+                        //  System.out.println(nextNode+"next node");
+                        dp[nxtNode] = 1+ dp[currNode];
+                        q.add(nxtNode);
+                    }
+                }
+            }
+        }
+        
+        return dp[num2];
+    }
+
+    public static void main(String[] args) {
+        int num1 = 1033;
+        int num2 = 8179;
+        System.out.println(solve(num1, num2));
+    }
+}
---
+++ Leetcode_Problems/p25_1.java
@@ -0,0 +1,24 @@
+public class p25_1 {
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[len1][len2];
+    }
+
+    public static void main(String[] args) {
+        String text1 = "abcde", text2 = "ace";
+        System.out.println(longestCommonSubsequence(text1, text2));
+    }
+}
