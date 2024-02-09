a7bf805d8ceb97524157696283ccd35a789a7a0a
RDX-Rudra <dasrudra234@gmail.com>
2024-02-10 01:53:45 +0530

    dsa

---
+++ Leetcode_Problems/p9_2.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class p9_2 {
+    public static List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.sort(nums);
+
+        int maxSize = 1, maxIndex = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    if (dp[i] > maxSize) {
+                        maxSize = dp[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int num = nums[maxIndex];
+        for (int i = maxIndex; i >= 0; i--) {
+            if (num % nums[i] == 0 && dp[i] == maxSize) {
+                result.add(nums[i]);
+                num = nums[i];
+                maxSize--;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
+        System.out.println(largestDivisibleSubset(nums));
+
+    }
+}
