f877a041ed445817ea16afaadd17acaca8553df1
RDX-Rudra <dasrudra234@gmail.com>
2024-03-13 22:20:20 +0530

    spring

--- SpringBoot/hello/src/main/java/com/rdxrudra/hello/HelloController.java
+++
@@ -1,9 +0,0 @@
-package com.rdxrudra.hello;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-public class HelloController {
-    public String hello(){
-        return "Hello, World";
-    }
-}
