e0a64289af7b7275d0b86245363383201ab0ee7a
RDX-Rudra <dasrudra234@gmail.com>
2024-03-21 13:32:37 +0530

    spring

--- quizapp/src/main/java/com/rdx/quizapp/QuizappApplication.java
+++ quizapp/src/main/java/com/rdx/quizapp/QuizappApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.rdx.quizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class QuizappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(QuizappApplication.class, args);
 	}
 
 }
--- quizapp/src/test/java/com/rdx/quizapp/QuizappApplicationTests.java
+++ quizapp/src/test/java/com/rdx/quizapp/QuizappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.rdx.quizapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class QuizappApplicationTests {
 
 	@Test
 	void contextLoads() {
