9037e566d89123ecd3737d06efe0c3b06ac3e169
RDX-Rudra <dasrudra234@gmail.com>
2024-03-24 23:22:54 +0530

    java

--- feed/listMathodes.java
+++ feed/listMathodes.java
@@ -1,4 +1,12 @@
-public class listMathodes {
+import java.util.Locale;
+import java.util.*;
+public interface listMathodes {
 
     
+    static void main(String[] args) {
+        var list = List.of("java", "Rdx", "fuck");
+        Locale.setDefault(Locale.CANADA);
+        var message = ListFormat.getInstance().format(list);
+        System.out.println(message);
+    }
 }
