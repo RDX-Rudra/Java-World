79954aaf51f997da9794f3620a060be38ab463c9
RDX-Rudra <dasrudra234@gmail.com>
2024-03-31 10:30:04 +0530

    dsa

--- Leetcode_Problems/P31_3_2444.java
+++ Leetcode_Problems/P31_3_2444.java
@@ -1,8 +1,31 @@
 public class P31_3_2444 {
-    public long countSubarrays(int[] nums, int minK, int maxK) {
-        
+    public static long countSubarrays(int[] nums, int minK, int maxK) {
+        int left=0, right=0;
+        long ans = 0;
+        boolean findmin = false, findmax = false;
+        while (right <nums.length) {
+            if(nums[right]<=maxK && nums[right]>=minK){
+                if(nums[right] == minK)
+                    findmin = true;
+                if(nums[right] == maxK)
+                    findmax = true;
+                if(findmax == true && findmin == true){
+                    ans ++;
+                }
+                right++;
+            }
+            else{
+                right++;
+                left = right;
+                findmax = false;
+                findmin = false;
+            }
+        }
+        return ans;
     }
     public static void main(String[] args) {
-        
+        int[] nums = {1,1,1,1};
+        int minK = 1, maxK = 1;
+        System.out.println(countSubarrays(nums, minK, maxK));
     }
 }
