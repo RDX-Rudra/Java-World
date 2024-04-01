e78d03c1cb87cea9df9a60a17d96a07783ca8d20
RDX-Rudra <dasrudra234@gmail.com>
2024-04-01 08:57:37 +0530

    leetcode 58

---
+++ Leetcode_Problems/P1_4.java
@@ -0,0 +1,13 @@
+public class P1_4 {
+    public static int lengthOfLastWord(String s) {
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello world";
+        System.out.println(lengthOfLastWord(s));
+    }
+}
