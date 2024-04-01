7f83f7a404d20321dfba27edfb6cb1bdb7beb3a5
RDX-Rudra <dasrudra234@gmail.com>
2024-04-01 09:09:13 +0530

    dsa

--- Leetcode_Problems/P1_4.java
+++ Leetcode_Problems/P1_4.java
@@ -1,13 +1,22 @@
 public class P1_4 {
     public static int lengthOfLastWord(String s) {
-        int n = s.length();
-        for(int i=0; i<n; i++){
-            
+        s = s.trim();
+        
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
         }
+        
+        return length;
     }
 
     public static void main(String[] args) {
-        String s = "Hello world";
+        String s = "   fly me   to   the moon  ";
         System.out.println(lengthOfLastWord(s));
     }
 }
