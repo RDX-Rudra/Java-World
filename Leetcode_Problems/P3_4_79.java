00285c7e4821a575b7cf69f16b7ac9ea2386435e
RDX-Rudra <dasrudra234@gmail.com>
2024-04-03 20:25:44 +0530

    dsa

--- Leetcode_Problems/P3_4_79.java
+++ Leetcode_Problems/P3_4_79.java
@@ -1,13 +1,57 @@
 public class P3_4_79 {
     public static boolean exist(char[][] board, String word) {
-        int n= word.length();
-        int i=0, k=0;
-        while (i<board.length && k<n) {
-            int j =0;
-            while (j<board[i].length) {
-                
+        int n = board.length, m = board[0].length;
+        boolean[][] visited = new boolean[n][m];
+        char[] wordChar = word.toCharArray();
+        if (wordChar.length > n * m)
+            return false;
+        int counts[] = new int[256];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                counts[board[i][j]]++;
             }
         }
+        int len = wordChar.length;
+        for(int i=0; i<len/2; i++)
+        {
+            if(counts[wordChar[i]]>counts[wordChar[len-1-i]]){
+                for(int j=0; j<len/2; j++)
+                {
+                    char temp = wordChar[j];
+                    wordChar[j]=wordChar[len-1-j];
+                    wordChar[len-1-j]=temp;
+                }
+                break;
+            }
+        }
+        for (char c : wordChar) {
+            if (--counts[c] < 0)
+                return false;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visit(board, wordChar, 0, i, j, n, m, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean visit(char[][] board, char[] word, int start, int x, int y,
+            int n, int m, boolean[][] visited) {
+        if (start == word.length)
+            return true;
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y])
+            return false;
+        if (word[start] != board[x][y])
+            return false;
+        visited[x][y] = true;
+        boolean found = visit(board, word, start + 1, x + 1, y, n, m, visited)
+                || visit(board, word, start + 1, x - 1, y, n, m, visited)
+                || visit(board, word, start + 1, x, y + 1, n, m, visited)
+                || visit(board, word, start + 1, x, y - 1, n, m, visited);
+        visited[x][y] = false;
+        return found;
     }
 
     public static void main(String[] args) {
