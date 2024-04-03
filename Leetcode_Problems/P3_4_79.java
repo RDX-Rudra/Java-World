b3d268461568e3212eb235affbf5cfae6bf2825c
RDX-Rudra <dasrudra234@gmail.com>
2024-04-03 18:43:18 +0530

    leetcode 79

---
+++ Leetcode_Problems/P3_4_79.java
@@ -0,0 +1,11 @@
+public class P3_4_79 {
+    public static boolean exist(char[][] board, String word) {
+        
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {{"A","B","C","E"},{"S","F","C","S"},{"A","D","E","E"}};
+        String word = "ABCCED";
+        System.out.println(exist(board, word));
+    }
+}
