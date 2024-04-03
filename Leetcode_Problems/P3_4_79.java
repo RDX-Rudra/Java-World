dad3b2d2aba3619bdd075744b1ac4bab20987e05
RDX-Rudra <dasrudra234@gmail.com>
2024-04-03 20:14:03 +0530

    dsa

--- Leetcode_Problems/P3_4_79.java
+++ Leetcode_Problems/P3_4_79.java
@@ -1,6 +1,13 @@
 public class P3_4_79 {
     public static boolean exist(char[][] board, String word) {
-        
+        int n= word.length();
+        int i=0, k=0;
+        while (i<board.length && k<n) {
+            int j =0;
+            while (j<board[i].length) {
+                
+            }
+        }
     }
 
     public static void main(String[] args) {
