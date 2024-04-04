49bbabded496af1ad4b16174e3cae02dd498784d
RDX-Rudra <dasrudra234@gmail.com>
2024-04-04 09:54:05 +0530

    dsa

--- Leetcode_Problems/P4_4_1614.java
+++ Leetcode_Problems/P4_4_1614.java
@@ -1,5 +1,28 @@
+import java.util.Stack;
+
 public class P4_4_1614 {
     public static int maxDepth(String s) {
-        
+        Stack<Character> store = new Stack<>();
+        int maxdept = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                store.push(s.charAt(i));
+                count++;
+                maxdept = Math.max(maxdept, count);
+            }
+            else if(s.charAt(i)==')'){
+                store.pop();
+                count--;
+            }
+            else
+                continue;
+        }
+        return maxdept;
+    }
+
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        System.out.println(maxDepth(s));
     }
 }
