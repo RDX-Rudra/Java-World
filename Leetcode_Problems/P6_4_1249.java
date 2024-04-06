2a1e3fcf136e1b848cc5099da72e1682d3e1f6cc
RDX-Rudra <dasrudra234@gmail.com>
2024-04-06 21:23:37 +0530

    dsa

--- Leetcode_Problems/P6_4_1249.java
+++ Leetcode_Problems/P6_4_1249.java
@@ -3,7 +3,28 @@ import java.util.Stack;
 public class P6_4_1249 {
     public static String minRemoveToMakeValid(String s) {
         Stack<Character> store = new Stack<>();
-        
+        char[] arr = s.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == '(')
+                store.push(arr[i]);
+            if(arr[i] == ')'){
+                if(!store.isEmpty()){
+                    store.pop();
+                }
+                else{
+                    arr[i] = ' ';
+                }
+            }
+        }
+        while()
+        StringBuilder result = new StringBuilder();
+        for(char c : arr){
+            if(c == ' ')
+                continue;
+            else
+                result.append(c);
+        }
+        return result.toString();
     }
     public static void main(String[] args) {
         String s = "lee(t(c)o)de)";
