f6595b3f3c125aa035da312334136ce18460303f
RDX-Rudra <dasrudra234@gmail.com>
2024-04-06 20:48:28 +0530

    leetcode 1249

---
+++ Leetcode_Problems/P6_4_1249.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class P6_4_1249 {
+    public static String minRemoveToMakeValid(String s) {
+        Stack<Character> store = new Stack<>();
+        
+    }
+    public static void main(String[] args) {
+        String s = "lee(t(c)o)de)";
+        System.out.println(minRemoveToMakeValid(s));
+    }
+}
