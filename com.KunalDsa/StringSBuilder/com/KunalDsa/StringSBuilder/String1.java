4bddc11f2da0af893faabfbedcf93a8e11996f67
RDX-Rudra <dasrudra234@gmail.com>
2024-05-15 19:45:24 +0530

    java

--- com.KunalDsa/StringSBuilder/com/KunalDsa/StringSBuilder/String1.java
+++ com.KunalDsa/StringSBuilder/com/KunalDsa/StringSBuilder/String1.java
@@ -1,3 +1,8 @@
 public class String1 {
-    
+    public static void main(String[] args) {
+        // int[] arr = {1,2,3,4};
+        // int num = 10;
+        String name = "Rudra";
+        System.out.println(name);
+    }
 }
