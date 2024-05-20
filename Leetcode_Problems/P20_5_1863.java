4c8436de73c5bf62322ee792f80b3cc5172c792d
RDX-Rudra <dasrudra234@gmail.com>
2024-05-21 01:57:16 +0530

    dsa

--- Leetcode_Problems/P20_5_1863.java
+++ Leetcode_Problems/P20_5_1863.java
@@ -1,9 +1,23 @@
 public class P20_5_1863 {
     public static int subsetXORSum(int[] nums) {
-        
+        int n = nums.length;
+        int totalSum = 0;
+        // Iterate through all possible subsets
+        for (int i = 0; i < (1 << n); i++) {
+            int subsetXor = 0;
+            for (int j = 0; j < n; j++) {
+                // Check if the j-th element is in the i-th subset
+                if ((i & (1 << j)) != 0) {
+                    subsetXor ^= nums[j];
+                }
+            }
+            totalSum += subsetXor;
+        }
+        return totalSum;
     }
     public static void main(String args[]){
-
+        int[] nums = {1, 3};
+        System.out.println(subsetXORSum(nums));
     }
 
 }    
