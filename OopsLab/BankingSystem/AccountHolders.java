6a33f2d19f14eeb291f88d935f9e03c3f1d3b186
RDX-Rudra <dasrudra234@gmail.com>
2024-12-03 21:11:19 +0530

    oops

---
+++ OopsLab/BankingSystem/AccountHolders.java
@@ -0,0 +1,12 @@
+package OopsLab.BankingSystem;
+
+public class AccountHolders {
+    String holderName;
+    int accountNum;
+    String typeOfAcc;
+    int initialBal;
+    
+    AccountHolders(){
+
+    }
+}
