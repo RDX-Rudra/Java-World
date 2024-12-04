226d966f635b44a030739e8ea41ebe835ff2b4ce
RDX-Rudra <dasrudra234@gmail.com>
2024-12-04 20:35:56 +0530

    Amazon Spring '23 High

--- AmazonSpringHigh/OptimalPartiStr2405.java
+++ AmazonSpringHigh/OptimalPartiStr2405.java
@@ -1,11 +1,26 @@
 package AmazonSpringHigh;
 
+import java.util.HashMap;
+
 public class OptimalPartiStr2405 {
-    public int partitionString(String s) {
-        
+    public static int partitionString(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int count=1;
+        for(int i=0; i<s.length(); i++){
+            if(map.get(s.charAt(i))==null)
+                map.put(s.charAt(i), 1);
+            else{
+                count++;
+                map.clear();
+                map.put(s.charAt(i), 1);
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        
+        String s="abacaba";
+        int ans = partitionString(s);
+        System.out.println(ans);
     }
 }
