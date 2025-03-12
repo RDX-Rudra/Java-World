059323d992b8fa03d1734c789f3edd63d9dd460b
Rudra Das <dasrudra234@gmail.com>
2025-03-12 08:50:28 +0530

    leetcode

---
+++ Leetcode_Problems/P11_3_1358.java
@@ -0,0 +1,43 @@
+public class P11_3_1358 {
+    public static int numberOfSubstrings(String s) {
+        int len = s.length();
+        int left = 0, right = 0;
+        // Track frequency of a, b, c
+        int[] freq = new int[3];
+        int total = 0;
+
+        while (right < len) {
+            // Add character at right pointer to frequency array
+            char curr = s.charAt(right);
+            freq[curr - 'a']++;
+
+            // While we have all required characters
+            while (hasAllChars(freq)) {
+                // All substrings from current window to end are valid
+                // Add count of these substrings to result
+                total += len - right;
+
+                // Remove leftmost character and move left pointer
+                char leftChar = s.charAt(left);
+                freq[leftChar - 'a']--;
+                left++;
+            }
+
+            right++;
+        }
+
+        return total;
+    }
+
+    private static boolean hasAllChars(int[] freq) {
+        // Check if we have at least one of each character
+        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
+    }
+    
+
+    public static void main(String[] args) {
+        String s = "abcabc";
+        int ans = numberOfSubstrings(s);
+        System.out.println(ans);
+    }
+}
