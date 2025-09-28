5cdce936d737b531609068e65bc43dcae1da2969
Rudra Das <dasrudra234@gmail.com>
2025-09-28 17:57:26 +0530

    graph

--- feed/GraphApnaColl/Graph1.java
+++ feed/GraphApnaColl/Graph1.java
@@ -11,34 +11,31 @@ public class Graph1 {
         }
     }
 
-    public static void createGraph(List<ArrayList<Edge>> graph) {
-        for (int i = 0; i < graph.size(); i++) {
-            graph.set(i, new ArrayList<Edge>());
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
         }
 
-        graph.get(0).add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 2));
 
-        graph.get(1).add(new Edge(1, 2));
-        graph.get(1).add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
 
-        graph.get(2).add(new Edge(2, 0));
-        graph.get(2).add(new Edge(2, 1));
-        graph.get(2).add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
 
-        graph.get(3).add(new Edge(3, 1));
-        graph.get(3).add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
     }
 
     public static void main(String[] args) {
         int v = 4;
-        List<ArrayList<Edge>> graph = new ArrayList<>(v);
-        for (int i = 0; i < v; i++) {
-            graph.add(null);
-        }
+        ArrayList<Edge>[] graph = (ArrayList<Edge>[]) new ArrayList[v];
 
         createGraph(graph);
 
-        for (Edge e : graph.get(2)) {
+        for (Edge e : graph[2]) {
             System.out.println("Source: " + e.src + ", Destination: " + e.dest);
         }
     }
