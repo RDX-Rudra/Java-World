4aca607e6eaf0125cd644d0629978ff7e28504a3
Rudra Das <dasrudra234@gmail.com>
2026-07-13 18:57:38 +0530

    Update LongestIncSub.java
    sss

--- feed/DPgfg/LongestIncSub.java
+++ feed/DPgfg/LongestIncSub.java
@@ -1,7 +1,20 @@
 public class LongestIncSub {
+    private int lisHelper(int[] nums, int p, int c, int[][] dp){
+        if(c >= nums.length) return 0;
+        int dpP = p + 1; // map -1 -> 0 to avoid negative index
+        if(dp[dpP][c] != 0) return dp[dpP][c];
+        int take = 0;
+        if(p == -1 || nums[c] > nums[p]) {
+            take = 1 + lisHelper(nums, c, c + 1, dp);
+        }
+        int skip = lisHelper(nums, p, c + 1, dp);
+        return dp[dpP][c] = Math.max(take, skip);
+
+    }
+
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        
+        int[][] dp = new int[nums.length + 1][nums.length];
+        return lisHelper(nums, -1, 0, dp);
     }
 
     public static void main(String[] args) {
